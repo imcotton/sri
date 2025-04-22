50b5796e0a616d8fc804107edd9ea8b0f6657ca8
Cotton Hou <himcotton@gmail.com>
2025-04-22 12:21:15 +0800

    app.ts for Web UI

---
+++ src/app.ts
@@ -0,0 +1,193 @@
+import { encodeQR } from 'qr';
+
+import { main, type Info } from './main.ts';
+
+
+
+
+
+interface Observer <T> {
+    next  (value: T): void;
+    error (cause: Error): void;
+}
+
+
+
+
+
+export type Result = {
+    hash: string,
+    qr: {
+        svg: string,
+    },
+};
+
+
+
+
+
+export function make (
+
+        form: HTMLElement | null | undefined,
+        { next, error }: Observer<Result>,
+
+): () => void {
+
+    return hook(form, {
+
+        error,
+
+        async next (info) {
+
+            try {
+
+                const hash = await main(info);
+
+                const svg = encodeQR(hash, 'svg', { optimize: true });
+
+                next({ hash, qr: { svg } });
+
+            } catch (cause) {
+
+                error(failed(cause));
+
+            }
+
+        },
+
+    });
+
+}
+
+
+
+
+
+function hook (form: unknown, { next, error }: Observer<Info>) {
+
+    assert(form instanceof HTMLFormElement, 'invalid form');
+
+    const { abort, signal } = new AbortController();
+
+    form.addEventListener('submit', function (event) {
+
+        try {
+
+            event.preventDefault();
+
+                assert(event.target instanceof HTMLFormElement);
+
+            const data = new FormData(event.target);
+
+            const file = data.get('file');
+
+                assert(file instanceof File, 'invalid file');
+                assert(file.size > 0,          'empty file');
+
+            const task = () => file.arrayBuffer();
+
+            const format = data.get('format');
+
+                assert(format === 'base64'
+                    || format === 'base58'
+                    || format === 'hex'
+                , 'invalid format');
+
+            const algorithm = data.get('algorithm');
+
+                assert(algorithm === '1'
+                    || algorithm === '256'
+                    || algorithm === '384'
+                    || algorithm === '512'
+                , 'invalid algorithm');
+
+            const prefix = data.get('prefix') === 'on';
+
+            next({ format, algorithm, task, prefix });
+
+        } catch (cause) {
+
+            error(failed(cause));
+
+        }
+
+    }, { signal });
+
+    return () => abort();
+
+}
+
+
+
+
+
+function failed (cause: unknown) {
+
+    if (cause instanceof Error) {
+        return cause;
+    }
+
+    return new Error('unknown', { cause });
+
+}
+
+
+
+
+
+function assert (expr: unknown, msg = 'fail assertion'): asserts expr {
+
+    if (!expr) {
+        throw new Error(msg);
+    }
+
+}
+
+
+
+
+
+function nmap <A, B> (
+
+        f: (a: A) => B,
+        a: A | undefined | null,
+
+): B | undefined {
+
+    if (a != null) {
+        return f(a);
+    }
+
+}
+
+
+
+
+
+export function byId <T> (
+
+        id: string,
+        f: (elm: HTMLElement) => T,
+
+): T | undefined {
+
+    return nmap(f, document.getElementById(id));
+
+}
+
+
+
+
+
+export function removeAttribute (attr: string): (elm: HTMLElement) => void {
+
+    return function (elm) {
+
+        if (elm.hasAttribute(attr)) {
+            elm.removeAttribute(attr);
+        }
+
+    }
+
+}
+
